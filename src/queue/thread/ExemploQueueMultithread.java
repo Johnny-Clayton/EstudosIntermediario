3b4a06760518c1a88788ebfac1a9d730670c536d
Johnny <delga300@hotmail.com>
2023-03-22 21:06:45 -0300

    Remoção de comentarios

--- src/queue/thread/ExemploQueueMultithread.java
+++ src/queue/thread/ExemploQueueMultithread.java
@@ -5,12 +5,11 @@ import java.util.Queue;
 
 public class ExemploQueueMultithread {
     public static void main(String[] args) {
-        // Cria uma Queue compartilhada entre as threads
+    	
         Queue<String> filaMensagens = new LinkedList<>();
 
-        // Cria a thread produtora
         Thread produtora = new Thread(() -> {
-            // Produz algumas mensagens e as adiciona na fila
+        	
             for (int i = 1; i <= 5; i++) {
                 String mensagem = "Mensagem " + i;
                 filaMensagens.offer(mensagem);
@@ -18,9 +17,8 @@ public class ExemploQueueMultithread {
             }
         });
 
-        // Cria a thread consumidora
         Thread consumidora = new Thread(() -> {
-            // Consome as mensagens da fila
+        	
             while (true) {
                 if (!filaMensagens.isEmpty()) {
                     String mensagem = filaMensagens.poll();
@@ -29,7 +27,6 @@ public class ExemploQueueMultithread {
             }
         });
 
-        // Inicia as threads
         produtora.start();
         consumidora.start();
     }
